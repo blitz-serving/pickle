b23709b8bcf9ea540b34b2dc5118dde9b531a758
Wang Haotian <WangHaotian013@outlook.com>
2025-01-10 07:56:45 +0000

    minor: cmakelists.txt

---
+++ examples/thread_controller.cpp
@@ -0,0 +1,62 @@
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+
+class PollingThreads {
+public:
+    void add_thread(std::thread t, std::function<void()> stop_function) {
+        background_threads_.emplace_back(std::move(t));
+        stop_functions_.emplace_back(std::move(stop_function));
+    }
+
+    explicit PollingThreads() {
+        std::cout << "PollingThreads::PollingThreads()" << std::endl;
+    };
+
+    ~PollingThreads() {
+        std::cout << "PollingThreads::~PollingThreads()" << std::endl;
+
+        for (auto& stop_function : stop_functions_) {
+            if (stop_function) {
+                stop_function();
+            }
+        }
+
+        for (auto& t : background_threads_) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+
+        std::cout << "PollingThreads::~PollingThreads() done" << std::endl;
+    }
+
+private:
+    PollingThreads& operator=(const PollingThreads&) = delete;
+    PollingThreads(const PollingThreads&) = delete;
+    PollingThreads& operator=(PollingThreads&&) = delete;
+    PollingThreads(PollingThreads&&) = delete;
+
+    std::vector<std::thread> background_threads_;
+    std::vector<std::function<void()>> stop_functions_;
+};
+
+PollingThreads gPollingThreads;
+
+int main() {
+    auto flag = std::make_shared<std::atomic<bool>>(false);
+    auto t = std::thread([flag]() {
+        while (!flag->load()) {
+            std::cout << "Polling..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    gPollingThreads.add_thread(std::move(t), [flag]() { flag->store(true); });
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    return 0;
+}
